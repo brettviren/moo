6dced81d49980da0adc04e92e5170e52b6aaafc4
Brett Viren <brett.viren@gmail.com>
2019-11-17 19:35:08 -0500

    Generate plantuml class diagrams from message models

--- moo.py
+++ moo.py
@@ -7,16 +7,19 @@ import sys
 import jsonnet
 from jinja2 import Environment, FileSystemLoader
 
-def render_protobuf(template, params):
-    env =  Environment(loader = FileSystemLoader(os.path.dirname(template)))
+def render_default(template, params):
+    env =  Environment(loader = FileSystemLoader(os.path.dirname(template)),
+                       extensions=['jinja2.ext.do'])
+                       
     tmpl = env.get_template(os.path.basename(template))
     return tmpl.render(**params)
+
 
 
 for one in sys.argv[1:]:
     dat = jsonnet.load(one)
 
-    render = eval("render_" + dat["renderer"])
+    render = eval("render_" + dat.get("renderer", "default"))
     text = render(dat["template"], dat["params"])
     output = dat["artifact"]
     open(dat["artifact"], 'wb').write(text.encode('utf-8'))
---
+++ ptmp-msg-classes-v0.jsonnet
@@ -0,0 +1,13 @@
+
+local pb = import "ptmp-proto-v0.jsonnet";
+local zmq = import "ptmp-zmq-v0.jsonnet";
+
+{
+    template: "classes.plantuml.j2",
+    artifact: "ptmp-msg-classes.plantuml",
+    params: {
+        messages: [m {namespace: "pb"} for m in pb]
+            + [m {namespace: "zmq"} for m in zmq],
+    },
+
+}
