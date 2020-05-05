8a4a4f6b1a08b7493759df50197d1703ac790c58
Brett Viren <brett.viren@gmail.com>
2020-05-04 21:35:24 -0400

    Checkpoint

---
+++ src/mex-ctxsml.cpp
@@ -0,0 +1,36 @@
+// This file is generated, you better copy it to somewhere else if you
+// edit it.
+
+#include "mex/ctxsml.hpp"
+
+// guards
+
+// actions
+void mex::CtxSml::do_bind(const mex::bind& event)
+{
+    return;
+}
+void mex::CtxSml::do_conn(const mex::conn& event)
+{
+    return;
+}
+void mex::CtxSml::do_recv(const mex::echo& event)
+{
+    return;
+}
+void mex::CtxSml::do_send(const mex::echo& event)
+{
+    return;
+}
+void mex::CtxSml::do_recv(const mex::hear& event)
+{
+    return;
+}
+void mex::CtxSml::do_unlink(const mex::stop& event)
+{
+    return;
+}
+void mex::CtxSml::do_send(const mex::yodel& event)
+{
+    return;
+}
