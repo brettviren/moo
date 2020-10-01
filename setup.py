b00d95f7e72f245d5ac04669cbeafa90c8b13bec
Brett Viren <brett.viren@gmail.com>
2020-10-01 09:21:51 -0400

    Set version 0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="moo",
-    version="0.0.0",
+    version="0.0.1",
     author="Brett Viren",
     author_email="brett.viren@gmail.com",
     description="Model oriented objects",
