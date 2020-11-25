ffbf2e3db36be28fbb297c3a8a5a96746ff85ff4
Brett Viren <brett.viren@gmail.com>
2020-11-25 13:40:02 -0500

    git subrepo clone --force --branch=v1.0.2 https://github.com/fniessen/org-html-themes other/org-html-themes
    subrepo:
      subdir:   "other/org-html-themes"
      merged:   "3a1995b"
    upstream:
      origin:   "https://github.com/fniessen/org-html-themes"
      branch:   "v1.0.2"
      commit:   "3a1995b"
    git-subrepo:
      version:  "0.4.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a04d8c2"

--- other/org-html-themes/styles/bigblow/js/hideshow.js
+++ other/org-html-themes/styles/bigblow/js/hideshow.js
@@ -1,6 +1,6 @@
 // hideshow.js --- HideShow JS file
 //
-// Copyright (C) 2014-2020 All Right Reserved, Fabrice Niessen
+// Copyright (C) 2014 All Right Reserved, Fabrice Niessen
 //
 // This file is free software: you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as
@@ -310,8 +310,7 @@ function hsAddReviewingPanels() {
                             + '(out of ' +  nbReviewTotalTasks + ' tasks)<br>'
                             + 'Shortcuts: '
                             + '<span class="hsReviewButton" onclick="hsReviewTaskNext()">r (next)</span> - '
-                            + '<span class="hsReviewButton" onclick="hsReviewTaskPrev()">R (previous)</span><br>'
-                            + '<span class="hsReviewButton" onclick="hsReviewTaskPrev()">q (quit)</span>'
+                            + '<span class="hsReviewButton" onclick="hsReviewTaskPrev()">R (previous)</span>'
                             + '</div>');
         }
         else {
@@ -319,8 +318,7 @@ function hsAddReviewingPanels() {
                             + '(out of ' +  nbReviewTotalTasks + ' tasks)<br>'
                             + 'Shortcuts: '
                             + '<span class="hsReviewButton" onclick="hsReviewTaskNext()">r (next)</span> - '
-                            + '<span class="hsReviewButton" onclick="hsReviewTaskPrev()">R (previous)</span><br>'
-                            + '<span class="hsReviewButton" onclick="hsReviewTaskPrev()">q (quit)</span>'
+                            + '<span class="hsReviewButton" onclick="hsReviewTaskPrev()">R (previous)</span>'
                             + '</div>');
         }
     });
